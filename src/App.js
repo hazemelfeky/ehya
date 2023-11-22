f14fa6cd78b5814a0d2367b7091cb9e2cbdd2286
hazemelfeky <hazemelfeky25@gmail.com>
2023-11-22 21:40:58 +0200

    feat: add download section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Recommendation from "./components/Recommendation";
 import Items from "./components/Items";
+import Download from "./components/Download";
 function App() {
   return (
     <div className="app">
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Recommendation />
       <Items />
+      <Download />
     </div>
   );
 }
---
+++ src/components/Download.jsx
@@ -0,0 +1,21 @@
+const Download = () => {
+  return (
+    <div className="download">
+      <div className="download-container container">
+        <div className="download-img"></div>
+        <div className="download-details">
+          <h2 className="title">
+            Download Ehya now <br />
+            and Get Promo Everyday
+          </h2>
+          <div className="buttons">
+            <button></button>
+            <button></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Download;
